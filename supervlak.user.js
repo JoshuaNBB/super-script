0759650ad74107ffc8258f594136e95a599643fc
JoshuaNBB <tommysnbb@gmail.com>
2025-06-08 19:08:55 +0200

    Update supervlak.user.js

--- supervlak.user.js
+++ supervlak.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         SUPER VLAK 2
 // @namespace    https://github.com/JoshuaNBB
-// @version      5.1
+// @version      5.2
 // @description  Vlak s režimem BARBARKA: 25 LC + 1 šlechtic ve všech útocích. © J.o.s.h.u.a 2025 (ojeb)
 // @author       J.o.s.h.u.a
-// @match        https://*/game.php?*screen=place*Add commentMore actions
+// @match        https://*/game.php?*screen=place*
 // @match        https://*/game.php?village=*&screen=map*
 // @match        https://*/game.php?t=*
 // @updateURL    https://raw.githubusercontent.com/JoshuaNBB/super-script/main/supervlak.user.js
